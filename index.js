6ed2f6cad37bd218e69c62ca078cd98d173bfa1e
Ryan Hodges <ryanhodges47@gmail.com>
2022-10-01 22:42:25 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const image = document.getElementById("image")
+
+function ChangeToLandscape()
+{
+    image.src = "LandscapeOne.jpg";
+}
+
+function ChangeToPortrait()
+{
+    image.src = "PortraitOne.jpg";
+}
+
+function ChangeToCouple()
+{
+    image.src = "CoupleOne.jpg";
+}
